f11f1185f69244b721084e0b8bc6f5755e6a6078
adam-almeida <adamportifolio@gmail.com>
2022-09-20 20:12:05 -0300

    Feat: Implementando o server

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// Config json response
+app.use(express.json)
+
+// Solve Cors
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Rotes
+
+app.listen(port, () => console.log(`Server is runnig on port ${port}`) )
