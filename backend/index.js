904022eefdbc6c8272a6ab22413109e2f5f29f77
adam-almeida <adamportifolio@gmail.com>
2022-09-20 22:28:57 -0300

    Feat: Implementando rotas de usuario

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,30 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const UserRoutes = require('./routes/UserRoutes')
 
-const port = process.env.PORT || 5000
+
+const port = 5000
 
 const app = express()
 
-// Config json response
-app.use(express.json)
-
 // Solve Cors
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+// Config json response
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
 // Public folder for images
 app.use(express.static('public'))
 
 // Routes
-const userRoutes = require('./routes/User')
+app.use('/users', UserRoutes)
 
-app.use('/users', userRoutes)
+app.get('/ping', (req, res) => {
+    res.json({ pong: true })
+})
 
-app.listen(port, () => console.log(`Server is runnig on port ${port}`) )
+app.listen(port, () => console.log(`Server is runnig on port ${port}`))
