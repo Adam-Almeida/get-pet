8b7731662fa8101c5905a2067a0931cb6d70e172
adam-almeida <adamportifolio@gmail.com>
2022-09-20 23:51:20 -0300

    Feat: Criação de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const { validationResult, matchedData } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -15,7 +16,7 @@ module.exports = class UserController {
             res.status(422).json({
                 "error": {
                     "confirmPassword": {
-                        "msg": "As senhas informadas não conferem.",
+                        "msg": "A senha e a confrimação de senha não conferem.",
                         "param": "confirmPassword",
                         "location": "body"
                     }
@@ -24,6 +25,37 @@ module.exports = class UserController {
             return
         }
 
-        res.json(data)
+        const { name, phone, email, password } = data
+
+        const emailExists = await User.findOne({email})
+
+        if(emailExists){
+            res.status(422).json({
+                "error": {
+                    "email": {
+                        "msg": "Por favor, utilize outro email.",
+                        "param": "email",
+                    }
+                }
+            })
+            return
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
+
+        try {
+            const newUser = await user.save()
+            res.json(newUser)
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
     }
 }
