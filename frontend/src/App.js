3d2f58e203a5dd2ff76dc56d7a2623b726710513
adam-almeida <adamportifolio@gmail.com>
2022-09-27 01:19:21 -0300

    chore: criando context de usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,27 +8,30 @@ import Login from '../src/components/pages/Auth/Login'
 import Register from '../src/components/pages/Auth/Register'
 import Home from './components/pages/Home'
 import MainContainer from './components/layout/container/MainContainer'
+import { UserProvider } from './context/UserContext'
 
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <MainContainer>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        <Routes>
-          <Route path="" element={<p>Path not resolved</p>} />
-        </Routes>
-      </MainContainer>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <MainContainer>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Routes>
+            <Route path="" element={<p>Path not resolved</p>} />
+          </Routes>
+        </MainContainer>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
