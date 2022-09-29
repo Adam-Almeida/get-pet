29b274660d2218eb72a4975aa4d4347732fbcbb4
adam-almeida <adamportifolio@gmail.com>
2022-09-28 22:12:55 -0300

    chore: criando o hook de flash message

---
+++ frontend/src/hooks/useFlashMessage.js
@@ -0,0 +1,11 @@
+import bus from "../utils/bus"
+
+export default function useFlashMessage() {
+    const setFlashMessage = (msg, type) =>{
+        bus.emit('flash', {
+            message: msg,
+            type: type
+        })
+    }
+    return { setFlashMessage }
+}
